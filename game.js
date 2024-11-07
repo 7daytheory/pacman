6b898a93549dad47dbc23adeee1e317729256c0d
Matthew Lowe <matthewjlowe11@gmail.com>
2024-11-07 14:46:48 -0500

    Draw walls using map array and createRect()

--- game.js
+++ game.js
@@ -8,6 +8,11 @@ let createRect = (x, y, width, height, color) => {
     canvasContext.fillRect(x, y , width, height);
 }
 
+//frames per second
+fps = 30;
+let oneBlockSize = 20;
+let wallColor = "#342DCA";
+
 //21 cols - 23 rows 
 //if 1 wall, if 0 not wall
 let map = [
@@ -35,3 +40,35 @@ let map = [
     [1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ];
+
+let gameMap = () => {
+    update()
+    draw()
+}
+
+let update = () => {
+    //Add later
+}
+
+let draw = () => {
+    //Add later
+    drawWalls()
+}
+
+let gameInterval = setInterval(gameLoop, 1000 / fps);
+
+let drawWalls = () => {
+    for(let i = 0; i < map.length; i++) {
+        for(let j = 0; j < map[0].length; i++) {
+            if(map[i][j]) { //Then it's a wall
+                createRect(
+                    j * oneBlockSize, 
+                    i * oneBlockSize, 
+                    oneBlockSize, 
+                    oneBlockSize, 
+                    wallColor
+                )
+            }
+        }
+    }
+}
